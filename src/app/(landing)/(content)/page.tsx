e5a159477aeb9c7029b92db6dc44a9641462f3ea
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-04-28 15:02:01 +0200

    style: image test

--- src/app/(landing)/(content)/page.tsx
+++ src/app/(landing)/(content)/page.tsx
@@ -1,10 +1,23 @@
+import test from '@/public/test.png';
+import Image from 'next/image';
+
 export default async function Home() {
   return (
-    <main className="grid gird-cols-1 lg:grid-cols-3 gap-4">
-      <div className="bg-muted-foreground/5 w-full h-96 rounded-xl" />
-      <div className="bg-muted-foreground/5 w-full h-96 rounded-xl" />
-      <div className="bg-muted-foreground/5 w-full h-96 rounded-xl" />
-      <div className="bg-muted-foreground/5 w-full h-96 rounded-xl" />
+    <main className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+      <Image
+        src={test}
+        alt={''}
+        className="flex transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110 bg-red-600"
+        style={{ transform: 'translate3d(0, 0, 0)' }}
+        placeholder="blur"
+      />
+      <Image
+        src={test}
+        alt={''}
+        className="flex transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110 bg-red-600"
+        style={{ transform: 'translate3d(0, 0, 0)' }}
+        placeholder="blur"
+      />
     </main>
   );
 }
