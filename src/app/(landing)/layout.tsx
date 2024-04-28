c8b863cda4868aa228270d901d53d4f6a89e6dc8
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-04-28 13:31:24 +0200

    chore: added metadata

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,9 +1,23 @@
 import Footer from '@/src/components/navigation/footer/footer';
 import Header from '@/src/components/navigation/header';
 import MainNavigation from '@/src/components/navigation/main.navigation';
+import { Metadata } from 'next';
 
 interface HomeLayoutProps {
   children: React.ReactNode;
+}
+
+export async function generateMetadata({ params }): Promise<Metadata> {
+  return {
+    title: {
+      default: 'Findntravel',
+      template: `%s | Findntravel`,
+    },
+    description: 'Explore the blackforst from above and find the locations',
+    applicationName: 'Findntravel',
+    keywords: [],
+    creator: 'Tobias Gleiter, Yannick Buekers',
+  };
 }
 
 export default async function HomeLayout({ children }: HomeLayoutProps) {
