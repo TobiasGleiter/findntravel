1e650482737dcd040c5ffd461bdb7171c4a86b9c
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-05-07 09:34:57 +0200

    style: hero card

--- src/app/(lang)/(landing)/page.tsx
+++ src/app/(lang)/(landing)/page.tsx
@@ -1,4 +1,5 @@
 import test from '@/public/test.png';
+import { Icons } from '@/src/components/ui/icons';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -14,8 +15,24 @@ export default async function Home() {
   ];
 
   return (
-    <main className="mx-auto max-w-[1960px] p-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+    <main className="mx-auto max-w-[1960px]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-">
+        <div className="flex items-center justify-center row-span-1 bg-secondary rounded-md text-center p-4">
+          <div className="flex flex-col gap-4 items-center">
+            <Icons.tree className="w-8 h-8" />
+            <h1 className="flex items-center text-3xl font-bold">
+              Blackforest from{' '}
+              <span className="bg-primary text-background px-2 ml-1">
+                above
+              </span>
+            </h1>
+            <span className="text-xl">Explore and find.</span>
+            <p className="px-12 text-muted-foreground">
+              Explore the Blackforest from above and try to find the hidden
+              places.
+            </p>
+          </div>
+        </div>
         {images.map(({ id, public_id, blurDataUrl }) => (
           <Link
             key={id}
@@ -27,7 +44,7 @@ export default async function Home() {
             <Image
               src={public_id}
               alt="Next.js Conf photo"
-              className="transform rounded-md  brightness-90 transition will-change-auto group-hover:brightness-110"
+              className="transform  brightness-90 transition will-change-auto group-hover:brightness-110"
               style={{ transform: 'translate3d(0, 0, 0)' }}
               placeholder="blur"
               blurDataURL={blurDataUrl}
