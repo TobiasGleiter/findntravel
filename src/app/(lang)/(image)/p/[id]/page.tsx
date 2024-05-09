53f7599556c63d0c4dd099fc881520e4b51ec648
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-05-09 09:24:43 +0200

    chore: image details

--- src/app/(lang)/(image)/p/[id]/page.tsx
+++ src/app/(lang)/(image)/p/[id]/page.tsx
@@ -1,3 +1,10 @@
-export default function ImageDetailsPage() {
-  return <div>ImageDetailsPage</div>;
+import OptimizedImage from '@/src/components/images/OptimizedImage';
+import cloudinary from '@/src/utils/cloudinary';
+
+export default async function ImageDetailsPage({ params }) {
+  const { id } = params;
+  const { resources } = await cloudinary.v2.search.expression(id).execute();
+  const { public_id } = resources[0];
+
+  return <OptimizedImage src={public_id} width={720} height={1080} />;
 }
--- src/app/(lang)/(landing)/page.tsx
+++ src/app/(lang)/(landing)/page.tsx
@@ -1,13 +1,14 @@
 import LanguageButton from '@/src/components/buttons/language.button';
+import OptimizedImage from '@/src/components/images/OptimizedImage';
 import Copyright from '@/src/components/legal/copyright';
 import LegalLinks from '@/src/components/legal/legal.links';
 import { Icons } from '@/src/components/ui/icons';
 import cloudinary from '@/src/utils/cloudinary';
-import Image from 'next/image';
 import Link from 'next/link';
 
 interface CloudinaryResource {
   public_id: string;
+  filename: string;
   secure_url: string;
 }
 
@@ -21,7 +22,7 @@ export default async function Home() {
   return (
     <main className="mx-auto max-w-[1960px]">
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-        <div className="flex min-h-[400px] row-span-1  items-center justify-center bg-secondary rounded-md text-center p-4 ">
+        <div className="flex min-h-[600px] row-span-1  items-center justify-center bg-secondary rounded-md text-center p-4 ">
           <div className="flex flex-col gap-4 items-center">
             <Icons.tree className="w-8 h-8" />
             <h1 className="flex items-center text-3xl font-bold">
@@ -37,25 +38,22 @@ export default async function Home() {
             </p>
           </div>
         </div>
-        {images.map(({ public_id, secure_url }: CloudinaryResource) => (
+        {images.map(({ public_id, filename }: CloudinaryResource) => (
           <Link
             key={public_id}
-            href={`/?photoId=${public_id}`}
-            as={`/p/${public_id}`}
+            href={`/?photoId=${filename}`}
+            as={`/p/${filename}`}
             shallow
             className="after:content group relative block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
           >
-            <Image
-              src={secure_url}
+            <OptimizedImage
+              src={public_id}
               alt="Next.js Conf photo"
               className="transform  brightness-90 transition will-change-auto group-hover:brightness-110"
               style={{ transform: 'translate3d(0, 0, 0)' }}
               width={720}
-              height={480}
-              sizes="(max-width: 720px) 100vw,
-              (max-width: 1280px) 50vw,
-              (max-width: 1536px) 33vw,
-              25vw"
+              height={1080}
+              sizes="(max-width: 720px) 25vw"
             />
           </Link>
         ))}
