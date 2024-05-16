c33003f456700fad6854964deada6327c87021c6
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-05-16 09:31:01 +0200

    refactor: footer and logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 import { Icons } from './icons';
 
-export default function Logo() {
+export default function Logo({ children }) {
   return (
     <Link className="flex flex-col gap-4 items-center" href={'/'}>
       <div className="relative">
         <Icons.tree className="w-8 h-8" />
       </div>
       <h1 className="flex items-center text-3xl md:text-4xl font-bold">
-        Blackforest from
+        Black Forest from
         <span className="bg-primary text-background pl-1 pr-2 ml-2">above</span>
       </h1>
+      {children}
     </Link>
   );
 }
