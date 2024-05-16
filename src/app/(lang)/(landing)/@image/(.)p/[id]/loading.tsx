18b7802ea9cba6ca9e8990d1fc9483b25baa9778
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-05-16 08:52:50 +0200

    style: padding modal top

--- src/app/(lang)/(landing)/@image/(.)p/[id]/loading.tsx
+++ src/app/(lang)/(landing)/@image/(.)p/[id]/loading.tsx
@@ -10,8 +10,8 @@ export default function Loading() {
     >
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex items-start justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative items-center transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div className="flex items-start pt-20 justify-center container text-center sm:items-center sm:p-0">
+          <div className="relative items-center transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <button className="absolute right-0" role="close">
               <div className="m-2 rounded-full p-8">
                 <Icons.close className="w-6 h-6" />
--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -15,8 +15,8 @@ export function Modal({ children }: { children: React.ReactNode }) {
     >
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex items-start justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+        <div className="flex items-start pt-20 justify-center container text-center sm:items-center sm:p-0">
+          <div className="relative items-center transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <button
               onClick={() => {
                 router.back();
